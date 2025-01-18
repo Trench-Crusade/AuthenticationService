4dcfc7d93910c4efe958d0ee95ded3a43df8d706
Sedarius-1 <jakubkalinowski1234@gmail.com>
2025-01-18 23:16:23 +0100

    Added mapping for entities

--- src/main/java/org/tc/authservice/AuthServiceApplication.java
+++ src/main/java/org/tc/authservice/AuthServiceApplication.java
@@ -2,10 +2,12 @@ package org.tc.authservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EntityScan("org.tc.infrastructure.postgres.entities")
 public class AuthServiceApplication {
 
 	public static void main(String[] args) {
